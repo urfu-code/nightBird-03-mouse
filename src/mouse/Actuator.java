0ace87cdcb3e2e50e1d79767ec12c6d64c9f275a
Кузнецова Юлия <j7smith1735@gmail.com>
2014-04-08 20:00:06 +0600

    1

---
+++ src/mouse/Actuator.java
@@ -0,0 +1,52 @@
+package mouse;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import wood.Action;
+import wood.Direction;
+import wood.My_Wood;
+import wood.My_WoodLoader;
+import wood.Point;
+import wood.PrintWood;
+
+public class Actuator {
+	
+	public static void main(String[] args){
+		File file = new File("wood.txt");
+		My_WoodLoader loader = new My_WoodLoader();
+		PrintWood wood;
+		try {
+			wood = (PrintWood)loader.Load(new FileInputStream(file), System.out);
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Введите имя мышки:\n");
+			String name = scanner.nextLine();
+			System.out.println("Введите точки старта и финиша:\n");
+			int x;
+			int y;
+			Point finish;
+			Point start;
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			start = new Point(x,y);
+			x = scanner.nextInt();
+			y = scanner.nextInt();
+			finish = new Point(x,y);
+			wood.createWoodman(name, start, finish);
+			My_Mouse mouse = new My_Mouse();
+			Action action = Action.Ok;
+			while (action != Action.WoodmanNotFound){
+				Direction direction = mouse.NextMove(action);
+				action = wood.move(name, direction);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+		
+}
